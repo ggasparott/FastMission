c139d783e053460129c98b295c273681621f9309
Your Name <gabrielgasparotto45@gmail.com>
2026-02-16 18:48:58 -0300

    fix: arrumando chamada de rota dentro do frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -46,7 +46,7 @@ export const uploadCSV = async (file) => {
   const formData = new FormData()
   formData.append('file', file)
   
-  const response = await api.post('/upload', formData, {
+  const response = await api.post('/api/upload', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
